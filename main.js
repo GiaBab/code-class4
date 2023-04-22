88a9aeff7eafdf5d7a9eb8e5e906f65c69b3ae15
GiaBab <gabrielbianglino@gmail.com>
2023-04-22 15:16:44 -0300

    testing

---
+++ main.js
@@ -0,0 +1,10 @@
+import './t1.js' ;
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('prueba', 'root', '', {
+  host: 'localhost',
+  dialect: 'mariadb' /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+});
+
+createUser("Juan","miguel")
